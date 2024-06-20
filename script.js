eeb6e6eaaa224dfd376b54265e5028f722ccc70a
Bootcamp QA <mybootcampqa@gmail.com>
2024-06-20 20:47:38 +0200

    added data cy

--- script.js
+++ script.js
@@ -1,6 +1,42 @@
+let respuestasCorrectas = ['A','B','C','A'];
+
+function imprimir(mensaje, id) {
+    document.getElementById(id).innerText = mensaje;
+}
+
 function verMensajeBienvenida() {
     let nombre = document.getElementById("welcomename").value;
     let mensaje = "Welcome " + nombre;
+    imprimir(mensaje,"welcomemessage");
+    
+}
+function corregirTodas(numPregunta,respuestaSeleccionada) {
+    let respuestaCorrecta = respuestasCorrectas[numPregunta-1];
+    let mensaje = "";
+    let puntos = 0;
+    if(respuestaSeleccionada === respuestaCorrecta) {
+        puntos = 2;
+        mensaje = "Correct, " + puntos + " points.";
+    }
+    else if (respuestaSeleccionada === "") {
+        puntos = 0;
+        mensaje = "Incorrect, " + puntos + " points.";
+    }
+    else {
+        puntos = -1;
+        mensaje = "Incorrect, " + puntos + " points.";
+    }
 
-    document.getElementById("welcomemessage").innerText = mensaje;
+    imprimir(mensaje,"messagequestion"+numPregunta);
+}
+
+function corregir(numPregunta) {
+    let respuestaSeleccionada = document.getElementById("question"+numPregunta).value;
+   
+    corregirTodas(numPregunta,respuestaSeleccionada);
+}
+
+function corregir2(numPregunta) {
+    let respuestaSeleccionada = document.getElementById("question"+numPregunta).checked;
+    corregirTodas(numPregunta,respuestaSeleccionada);
 }
